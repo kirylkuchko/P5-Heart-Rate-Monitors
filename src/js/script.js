a8b4ee3abbf3be4367ddd0a29a0b663984b0e18d
Kiryl Kuchko <kuchkokirill@mail.ru>
2022-03-31 14:30:40 +0200

    Added section with carousel based on Slick and jQuery

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        infinite: true,
+        speed: 300,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.png"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.png"></img></button>',
+        responsive:[
+            {
+                breackpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
+});

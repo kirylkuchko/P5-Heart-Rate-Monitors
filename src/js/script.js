df064ef79f9cfb061702f541fa9fee5df6b837e8
Kiryl Kuchko <kuchkokirill@mail.ru>
2022-03-31 15:51:12 +0200

    Small visual fixes

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: true,
         speed: 300,
-        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left-arrow.png"></img></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right-arrow.png"></img></button>',
         responsive:[
